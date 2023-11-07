78338ed8e172c6571142764716b2eca455ce8105
최기환 <rlghks3004@gmail.com>
2023-11-07 12:16:14 +0900

    .

--- src/utils/mysql.js
+++ src/utils/mysql.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const dbInfo = {
   host: "dongnaecomm.cafe24app.com",
   port: "3306",
-  user: "	devcafe1",
+  user: "devcafe1",
   password: "dlatl13579!",
   database: "devcafe1",
 };
